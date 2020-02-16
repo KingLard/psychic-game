b24ee3c28f75b0683af02a94020f3b4d66a01ac3
Brett Landes <brettl0407@gmail.com>
2020-02-16 17:03:23 -0500

    created a function to update the index.html page displayed to the user after an event occurred

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,23 @@
 var wins = 0;
-var loses = 0;
+var losses = 0;
 var guessLeft = 9
 var guessSoFar = [];
 var userGuess;
 var computerGuess;
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var displayText = "Are you Psychic....Click any letter to start the game!!!";
+var displayText = "Are you a psychic? Click any letter to start the playing!";
+
+// create a function to display the results to the user
+function updateDisplay() {
+    document.querySelector("#displayText").innerHTML = displayText;
+    document.querySelector("#wins").innerHTML = wins;
+    document.querySelector("#losses").innerHTML = losses;
+    document.querySelector("#guessLeft").innerHTML = guessLeft;
+    document.querySelector("#guessSoFar").innerHTML = guessSoFar;
+};
+updateDisplay();
+
+
+
+
